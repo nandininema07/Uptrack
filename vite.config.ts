06c68e27a9ba7027a4ab5ca8f6e33c9ba1b64fd4
nandininema07 <nandini.nema027@gmail.com>
2025-08-10 23:08:28 +0530

    dist location fix

--- vite.config.ts
+++ vite.config.ts
@@ -26,9 +26,9 @@ export default defineConfig(async () => {
         "@assets": path.resolve(__dirname, "attached_assets"),
       },
     },
-    root: path.resolve(__dirname, "client"),
+    root: "client",
     build: {
-      outDir: path.resolve(__dirname, "dist"),
+      outDir: "../dist",
       emptyOutDir: true,
     },
     server: {
